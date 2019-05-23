89b66d5514ff5ecab29f4478acf5b4e6fa8756bd
shantiox <danaciocoiu78@gmail.com>
2019-05-23 11:33:48 +0300

    rest pojo student list

---
+++ spring-rest-demo/src/main/java/springdemo/rest/StdentRestController.java
@@ -0,0 +1,32 @@
+package springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+
+public class StdentRestController {
+
+	
+	// define endpoint for "/students" - return list of students
+	
+		@GetMapping("/students")
+		public List<Student> getStudents() {
+
+			List<Student> theStudents = new ArrayList<>();
+			
+			theStudents.add(new Student("Poornima", "Patel"));
+			theStudents.add(new Student("Mario", "Rossi"));
+			theStudents.add(new Student("Mary", "Smith"));		
+				
+			return theStudents;
+		}
+	
+}
