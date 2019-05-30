4c213a4734d4c705dc11df5bcf241540f7883ff6
shantiox <danaciocoiu78@gmail.com>
2019-05-30 10:21:10 +0300

    crud

--- hibernate-basic-cruddemo/src/main/java/com/home/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ hibernate-basic-cruddemo/src/main/java/com/home/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -26,7 +26,7 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	
 	
 	@Override
-	@Transactional
+	
 	public List<Employee> findAll() {
 
 		// get the current hibernate session
@@ -43,9 +43,50 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 		return employees;
 	}
 
+
+	@Override
+	public Employee findById(int theId) {
+		
+		// get the current hibernate session
+				Session currentSession = entityManager.unwrap(Session.class);
+				
+				// get the employee
+				Employee theEmployee =
+						currentSession.get(Employee.class, theId);
+				
+				// return the employee
+				return theEmployee;
+			}
+
+
+			@Override
+			public void save(Employee theEmployee) {
+
+				// get the current hibernate session
+				Session currentSession = entityManager.unwrap(Session.class);
+				
+				// save employee
+				currentSession.saveOrUpdate(theEmployee);
+			}
+
+
+			@Override
+			public void deleteById(int theId) {
+				
+				// get the current hibernate session
+				Session currentSession = entityManager.unwrap(Session.class);
+						
+				// delete object with primary key
+				Query theQuery = 
+						currentSession.createQuery(
+								"delete from Employee where id=:employeeId");
+				theQuery.setParameter("employeeId", theId);
+				
+				theQuery.executeUpdate();
+
 }
 
-
+}
 
 
 
---
+++ hibernate-basic-cruddemo/src/main/java/com/home/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.home.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.home.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+	
+public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
